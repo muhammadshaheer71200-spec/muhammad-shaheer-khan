8809c1b1212671935cc7a68a276a2ac83aed0523
muhammadshaheer71200-spec <muhammadshaheer71200@gmail.com>
2025-09-15 23:41:05 +0500

    Add files via upload

---
+++ mad libs.py
@@ -0,0 +1,17 @@
+# Mad Libs Game (Simple Print Version)
+
+print("Welcome to Mad Libs!")
+print("Fill in the blanks below:")
+
+# Taking inputs
+name = input("Enter a name: ")
+place = input("Enter a place: ")
+animal = input("Enter an animal: ")
+food = input("Enter a food: ")
+activity = input("Enter an activity: ")
+
+# Story output
+print("One day, " , name , " went to " , place , ".")
+print("There, " , name , " saw a " , animal , " eating " , food , ".")
+print(name , " was so surprised that they started " , activity , "!")
+print("It was the craziest day ever!")
